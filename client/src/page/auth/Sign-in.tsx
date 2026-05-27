bfa52a976c902f8822e56c478afbaad539ff1a96
peeepeepee <piyush1.61803@gmail.com>
2026-05-28 00:02:55 +0530

    fixed frontend bugs

--- client/src/page/auth/Sign-in.tsx
+++ client/src/page/auth/Sign-in.tsx
@@ -126,12 +126,6 @@ const SignIn = () => {
                                 <FormLabel className="dark:text-[#f1f7feb5] text-sm">
                                   Password
                                 </FormLabel>
-                                <a
-                                  href="#"
-                                  className="ml-auto text-sm underline-offset-4 hover:underline"
-                                >
-                                  Forgot your password?
-                                </a>
                               </div>
                               <FormControl>
                                 <Input
@@ -169,10 +163,6 @@ const SignIn = () => {
               </Form>
             </CardContent>
           </Card>
-          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-            By clicking continue, you agree to our{" "}
-            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-          </div>
         </div>
       </div>
     </div>
